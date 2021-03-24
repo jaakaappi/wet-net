607cce66c89a1b0cea21f46cdfa74f192f2652a0
Esa Niemi <diapaasi@gmail.com>
2021-03-24 18:22:20 +0200

    Removed d1mini, working dht11

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,48 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
-
-#ifdef ESP32
+#include "DHT.h"
 #include "SPIFFS.h"
 #include "Update.h"
 #include "WiFi.h"
-#endif
-#ifdef ESP8266
-#include "ESP8266WiFi.h"
-#include "LittleFS.h"
-#endif
+
+#define DHTPIN 23
+#define DHTTYPE DHT11
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+
   u8g2.begin();
-  u8g2.clearBuffer();                    // clear the internal memory
-  u8g2.setFont(u8g2_font_logisoso28_tr); // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
-  u8g2.drawStr(8, 29, "MYBOTIC");        // write something to the internal memory
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(8, 29, "MYBOTIC");
   u8g2.sendBuffer();
+
+  dht.begin();
+
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
 }
